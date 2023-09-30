d246d6c7671ef4261681fe90898ea79c1c21fae0
varshahv15 <varshahv15@gmail.com>
2023-09-30 11:46:23 +1000

    Updated_Exercis_ Logging_Page
    -Updated the Exercise logging page

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-// Calories burned per minute for each type of exercise
 const exerciseCalories = {
-    running: 10,
-    swimming: 8,
-    cycling: 7,
-    walking: 5
-  };
-  
-  function calculateCalories() {
-    const exercise = document.getElementById("exercise").value;
-    const time = document.getElementById("time").value;
-    const caloriesBurned = exerciseCalories[exercise] * time;
-  
-    document.getElementById("result").innerHTML = `You have burned ${caloriesBurned} calories by doing ${time} minutes of ${exercise}.`;
-  }
-  
+  running: 10,
+  swimming: 8,
+  cycling: 7,
+  walking: 5
+};
+
+function changeBackground() {
+  const exercise = document.getElementById("exercise").value;
+  document.body.style.backgroundImage = `url('images/${exercise}.jpg')`;
+  document.getElementById("result").style.backgroundImage = `url('images/${exercise}.jpg')`;
+}
+
+function calculateCalories() {
+  const exercise = document.getElementById("exercise").value;
+  const time = document.getElementById("time").value;
+  const caloriesBurned = exerciseCalories[exercise] * time;
+
+  document.getElementById("result").innerHTML = `<span class="output-text">You have burned ${caloriesBurned} calories by doing ${time} minutes of ${exercise}.</span>`;
+}
